8f17de456bfcfa3b76fdd02aa2569083925bbc0d
zaksalazar <zsalazar.tv@gmail.com>
2023-03-11 11:40:48 -0800

    added utils

---
+++ server/utils/data.js
@@ -0,0 +1,29 @@
+const genericItems = [
+{
+    Name: "diapers",
+    category: "baby"
+  },
+  
+  {
+    name: "socks",
+    category: "clothing"
+  },
+  
+  {
+    name: "Propane",
+    category: "home goods"
+  },
+  
+  {
+    name: "Sleeping Bag",
+    category: "camping"
+  },
+  
+  {
+    name: "tooth brush",
+    category: "hygiene"
+  
+  },
+]
+
+module.exports = genericItems;
